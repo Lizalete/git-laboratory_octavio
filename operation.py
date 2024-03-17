c222e033a6e68f6ae95484369f2622420dd3376b
Octavio Navarro <octavio.navarro@tec.mx>
2024-03-17 12:48:40 -0600

    Changes

--- operation.py
+++ operation.py
@@ -2,7 +2,9 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the sum
+print(f'sum {a}+{b}={a+b}')
 
 # Display the subtraction
+print(f'sub {a}-{b}={a-b}')
 
 # Display multiplication
